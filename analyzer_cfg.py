ab425d32fc7228e3a89c9cd97899f3289bcb6e69
zhangjin <>
2009-10-21 16:44:45 +0000

    *** empty log message ***

--- analyzer_cfg.py
+++ analyzer_cfg.py
@@ -9,6 +9,8 @@ process.source = cms.Source("PoolSource",
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.MuonAnalyzer = cms.EDAnalyzer('Analyzer',
+                PrimaryVertices = cms.untracked.string("offlinePrimaryVertices"),
+                TriggerResultsTag = cms.untracked.InputTag("TriggerResults::HLT"),
                 triggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD","","HLT"),
                 hltFilterNames = cms.VInputTag(cms.InputTag("hltSingleMu15L3PreFiltered15","","HLT"),
                                                cms.InputTag("hltSingleMuIsoL3IsoFiltered9","","HLT"),
